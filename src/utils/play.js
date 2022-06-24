68fc5f2d4534e3ddef26c1930c67d6ef53b40bba
nibor51 <jonval.robin@gmail.com>
2022-06-24 17:29:06 +0200

    1 file modified, 1 deleted

--- src/utils/play.js
+++
@@ -1,8 +0,0 @@
-export const play = (e) => e.preventDefault();
-if (Number.isInteger(userNumber) && userNumber > 0 && userNumber <= 100) {
-  setRound((round) => round + 1);
-  setIsSame(userNumber == number);
-} else {
-  setError('Votre proposition doit être un nombre entre 1 et 100 !');
-}
-setUserNumber('');
